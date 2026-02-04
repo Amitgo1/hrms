c8b0e3ecf7e8c3e34c760c8e85a67fd9db1fc61b
Amit Goswami <amitgoswami089@gmail.com>
2026-02-04 20:28:35 +0530

    some changes

--- hrms_frontend/src/main.jsx
+++ hrms_frontend/src/main.jsx
@@ -3,11 +3,9 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import App from "./App";
-import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <Toaster position="top-right" />
     <App />
   </Provider>,
 );
