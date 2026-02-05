b330e4b63f49cfd9557e0c9ce647f8fa8114c2ee
Amit Goswami <amitgoswami089@gmail.com>
2026-02-05 09:08:31 +0530

    fix api issue

--- hrms_frontend/src/services/api.js
+++ hrms_frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://hrms-9-crzc.onrender.com/api/",
+  baseURL: import.meta.env.VITE_API_URL || "https://hrms-34.onrender.com/api/",
 });
 
 export default api;
